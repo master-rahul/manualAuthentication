ab706511a984fa979f9931f1141eb1adec86366e
Rahul Verma <extreme@Rahuls-MacBook-Air.local>
2023-02-10 09:58:30 +0530

    redo all

--- route/home.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authenticationController = require('../controller/authenticate');
-router.get('/',authenticationController.signIn);
-router.get('/signUp', authenticationController.signUp);
-
-module.exports = router;
